52eddaa248bd521bfc5f41074efb12b4e774118e
qinzhihao <scottzhq@163.com>
2016-12-16 14:40:20 +0800

    add groups/show

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,34 @@
+class GroupsController < ApplicationController
+
+  def index
+    @groups = Group.all
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.save
+
+    redirect_to groups_path
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end 
+
+
+
+
+      private
+
+      def group_params
+        params.require(:group).permit(:title, :description)
+      end
+
+
+
+
+end
