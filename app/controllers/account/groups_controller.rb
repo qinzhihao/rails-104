d16d2a32f025345c9a10e68e2124a633ccf2f726
qinzhihao <scottzhq@163.com>
2016-12-18 14:17:14 +0800

    implement edit post and delete post

--- app/controllers/account/groups_controller.rb
+++ app/controllers/account/groups_controller.rb
@@ -3,6 +3,6 @@ class Account::GroupsController < ApplicationController
 
   def index
     @groups = current_user.participated_groups
-  end
-  
+  end 
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,24 @@
 class PostsController < ApplicationController
-  before_filter :authenticate_user!, :only => [:new, :create]
+  before_filter :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
 
   def new
     @group = Group.find(params[:group_id])
     @post = Post.new
+  end
+
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "Updated success"
+    else
+      render :edit
+    end
   end
 
   def create
@@ -16,6 +31,13 @@ class PostsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to account_posts_path, alert: "Post deleted"
   end
 
 
